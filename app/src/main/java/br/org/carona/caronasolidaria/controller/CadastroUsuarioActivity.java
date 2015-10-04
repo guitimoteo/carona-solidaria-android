5e5a461f6b35d4e2938a804bc8dcbe31599c55f7
Guilherme <guitimoteo@yahoo.com.br>
2015-10-04 16:05:03 -0300

    Adicionando um cadastro de usuário

---
+++ app/src/main/java/br/org/carona/caronasolidaria/controller/CadastroUsuarioActivity.java
@@ -0,0 +1,21 @@
+package br.org.carona.caronasolidaria.controller;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import br.org.carona.caronasolidaria.R;
+
+public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+
+    }
+
+}
