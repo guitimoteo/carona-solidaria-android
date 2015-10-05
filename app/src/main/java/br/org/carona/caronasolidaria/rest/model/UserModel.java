71a6309f2db367d45f6f72ca069234715481a96e
Guilherme <guitimoteo@yahoo.com.br>
2015-10-04 21:03:19 -0300

    Adicionando a model a ser serializada para o formulário de cadastro de usuário

---
+++ app/src/main/java/br/org/carona/caronasolidaria/rest/model/UserModel.java
@@ -0,0 +1,62 @@
+package br.org.carona.caronasolidaria.rest.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Model a ser serializada e enviada via json
+ * User : {"nome":"nome","email":"exemplo@fatec.com.br","password":"exemplo@fatec.com.br"}
+ */
+public class UserModel {
+
+    /**
+     * Exemplo
+     * User : {"nome":"nome","email":"exemplo@fatec.com.br","password":"exemplo@fatec.com.br"}
+     */
+
+    @SerializedName("User")
+    private UserEntity User;
+
+    public void setUser(UserEntity User) {
+        this.User = User;
+    }
+
+    public UserEntity getUser() {
+        return User;
+    }
+
+    public static class UserEntity {
+        /**
+         * nome : nome
+         * email : asdf@fatec.com.br
+         * password : asdf@fatec.com.br
+         */
+
+        private String nome;
+        private String email;
+        private String password;
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+}
